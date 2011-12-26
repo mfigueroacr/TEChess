87d06724ce41fffc83be7bd209090c0057caca74
unknown <Kelvin@.(none)>
2011-12-26 10:57:41 -0600

    Form para loguearse

---
+++ ingresar.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>TeChess</title>
+<link href="css/css.css" rel="stylesheet" type="text/css" />
+
+</head>
+<body>
+        <form id="login" action="login.php" method="POST">
+        <fieldset id="login"> <legend> Ingreso </legend>
+<label>Nombre de Usuario:</label><input type="text" id="user" name="user" />
+<br />
+<br />
+
+<label>Contraseña:</label><input type="password"  id="pass" name="pass"/>
+<br />
+</fieldset>
+
+<br />
+
+<input type="submit" name="button" id="button" value="Enviar" />
+
+<input type="submit" name="Submit" value="Cancelar" onclick="location.href='index.php'"/></td>
+
+<button type="reset" id="reset_bt">Limpiar</button>
+
+    </form></div>
+
+</body></html>
