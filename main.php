7bcb533e011393dd9da94d52b0547ffefd3e7e6f
unknown <Kelvin@.(none)>
2011-12-26 19:37:51 -0600

    aqui es donde se debe escribir el codigo para el menu de los usuarios, recuerden que depende de los permisos

---
+++ main.php
@@ -0,0 +1,19 @@
+<?php
+include ("session.inc");
+check_login("");
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>TeChess</title>
+
+
+</head>
+<body>
+	<a href="logout.php">Logout </a></span>
+	<h1>TeChess logueado satisfactoriamente!!!</h1>
+
+</body>
+
+</html>
