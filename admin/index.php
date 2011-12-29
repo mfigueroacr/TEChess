aa91d1485431ef738e90ba87f57bca0284bc3630
unknown <Kelvin@.(none)>
2011-12-29 12:37:47 -0600

    cambio de html a php, para agregarle el control de session

--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,7 @@
+<?php
+include ("session.inc");
+check_login("administrador");
+?>
 <!DOCTYPE html>
 <html lang="en">
 	<head>
--- training/index.php
+++ training/index.php
@@ -1,3 +1,7 @@
+<?php
+include ("session.inc");
+check_login("");
+?>
 <!DOCTYPE html>
 <html lang="en">
 	<head>
