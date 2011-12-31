d82ea4508160550d56369d60d0cf17dff445c49a
unknown <Kelvin@.(none)>
2011-12-30 21:40:05 -0600

    clase para categoria nueva

--- tools/category.php
+++ tools/category.php
@@ -1,3 +1,15 @@
 <?php
-
+class categoy{
+	public $_link ;
+	
+	public function __construct (mysqli $link) {
+		#$this->$link = $link ;
+		$this->_link = $link;
+	}
+public function create_level($name, $description){
+	$html = "";
+$result = c_mysqli_call($this->_link, 'Create_Level', "'$name','$description'");
+return $result;
+}
+}
 ?>
