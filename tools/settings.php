7f9c304547a2419f11a260abbaead21dbf00716f
unknown <Kelvin@.(none)>
2012-01-10 13:29:25 -0600

    cambios en la base de datos

--- tools/settings.php
+++ tools/settings.php
@@ -1,10 +1,6 @@
 <?php
 define("SERVER", "127.0.0.1");
-<<<<<<< HEAD
-define("DB", "techess");
-=======
 define("DB", "TeChess");
->>>>>>> 4eec035a20b48de81f264b28b9e0bc710e73c67d
 define("USER", "root");
 define("PASSWORD", "");
 ?>
