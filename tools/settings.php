470ef30748c77a60ef87794b81f985074aaa634d
unknown <Kelvin@.(none)>
2012-01-23 15:05:42 -0600

    base de datos para heliohost

--- tools/settings.php
+++ tools/settings.php
@@ -1,7 +1,7 @@
 <?php
 // Archivo tools/settings.php
 define("SERVER", "localhost");
-define("DB", "techess");
-define("USER", "root");
-define("PASSWORD", "");
+define("DB", "techess_main");
+define("USER", "techess");
+define("PASSWORD", "qwerty");
 define("CODE", "d91bbf62a81f925ab82478c2339d8879");
